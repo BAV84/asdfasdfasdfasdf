9bc809f8928bbbcc953c81807b3fb6401c940739
Brian Vasquez <147463@s020rmc148-22.local>
2018-11-14 11:35:28 -0600

    Added a new file named MapForItinerary Changed import foundation -> import UIKit Also added a class

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Vasquez, Brian A on 11/14/18.
+//  Copyright © 2018 Vasquez, Brian A. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+    
+}
